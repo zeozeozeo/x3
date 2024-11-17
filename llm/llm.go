aaf116af26408b8ba6cad824ccbffeeb065671c1
zeozeozeo <108888572+zeozeozeo@users.noreply.github.com>
2024-11-17 21:23:24 +0500

    Revert "Perhaps...."
    This reverts commit ea162147bf7208fd66411f5e50974de09dc8e4f4.

--- llm/llm.go
+++ llm/llm.go
@@ -3,17 +3,17 @@ package llm
 import (
 	"context"
 	"errors"
+	"io"
 	"log/slog"
 	"os"
+	"strings"
 	"time"
 
-	azopenai "github.com/Azure/azure-sdk-for-go/sdk/ai/azopenai"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	openai "github.com/sashabaranov/go-openai"
 )
 
 var (
-	githubToken   = os.Getenv("X3ZEO_GITHUB_TOKEN")
-	errNoResponse = errors.New("no response")
+	githubToken = os.Getenv("X3ZEO_GITHUB_TOKEN")
 )
 
 const (
@@ -21,14 +21,9 @@ const (
 )
 
 const (
-	RoleUser      = "user"
-	RoleAssistant = "assistant"
+	RoleUser      = openai.ChatMessageRoleUser
+	RoleAssistant = openai.ChatMessageRoleAssistant
 )
-
-type Message struct {
-	Content string `json:"content"`
-	Role    string `json:"role"`
-}
 
 const (
 	ModelGpt4o                    = "gpt-4o"
@@ -43,16 +38,16 @@ const (
 )
 
 type Llmer struct {
-	Messages []Message `json:"messages"`
+	Messages []openai.ChatCompletionMessage `json:"messages"`
 }
 
 func NewLlmer() *Llmer {
 	return &Llmer{}
 }
 
-func newClient() (*azopenai.Client, error) {
-	cred := azcore.NewKeyCredential(githubToken)
-	return azopenai.NewClientWithKeyCredential(azureBaseURL, cred, nil)
+func newClient() *openai.Client {
+	config := openai.DefaultAzureConfig(githubToken, azureBaseURL)
+	return openai.NewClientWithConfig(config)
 }
 
 func (l *Llmer) NumMessages() int {
@@ -65,28 +60,12 @@ func (l *Llmer) TruncateMessages(max int) {
 	}
 }
 
-func (l *Llmer) AddMessage(message Message) {
-	l.Messages = append(l.Messages, message)
-}
-
-func (l *Llmer) convertMessageHistory() []azopenai.ChatRequestMessageClassification {
-	var messages []azopenai.ChatRequestMessageClassification
-
-	// WTF is this API O_o
-	for _, msg := range l.Messages {
-		switch msg.Role {
-		case RoleUser:
-			messages = append(messages, &azopenai.ChatRequestUserMessage{
-				Content: azopenai.NewChatRequestUserMessageContent(msg.Content),
-			})
-		case RoleAssistant:
-			messages = append(messages, &azopenai.ChatRequestAssistantMessage{
-				Content: azopenai.NewChatRequestAssistantMessageContent(msg.Content),
-			})
-		}
+func (l *Llmer) AddMessage(role, content string) {
+	msg := openai.ChatCompletionMessage{
+		Role:    role,
+		Content: content,
 	}
-
-	return messages
+	l.Messages = append(l.Messages, msg)
 }
 
 func (l *Llmer) RequestCompletion(model string) (string, error) {
@@ -97,80 +76,38 @@ func (l *Llmer) RequestCompletion(model string) (string, error) {
 		slog.Debug("    message", slog.String("role", msg.Role), slog.String("content", msg.Content))
 	}
 
-	client, err := newClient()
+	req := openai.ChatCompletionRequest{
+		Model:    model,
+		Messages: l.Messages,
+		Stream:   true,
+	}
+
+	completionStart := time.Now()
+	client := newClient()
+
+	stream, err := client.CreateChatCompletionStream(context.Background(), req)
 	if err != nil {
 		return "", err
 	}
+	defer stream.Close()
 
-	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(5*time.Minute))
-	defer cancel()
-	resp, err := client.GetChatCompletions(ctx, azopenai.ChatCompletionsOptions{
-		Messages:       l.convertMessageHistory(),
-		DeploymentName: &model,
-	}, nil)
-	if err != nil {
-		return "", err
-	}
-
-	for _, choice := range resp.Choices {
-		if choice.Delta != nil && choice.Delta.Content != nil {
-			slog.Debug("----------------------------------", slog.String("text", *choice.Delta.Content))
+	var text strings.Builder
+	for {
+		response, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			break
 		}
-	}
-
-	if len(resp.Choices) == 0 || resp.Choices[0].Message == nil || resp.Choices[0].Message.Content == nil {
-		return "", errNoResponse
-	}
-	slog.Debug("response message content", slog.String("text", *resp.Choices[0].Message.Content))
-	return *resp.Choices[0].Message.Content, nil
-
-	/*
-		completionStart := time.Now()
-		client := newClient()
-
-		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(5*time.Minute))
-		defer cancel()
-
-		resp, err := client.CreateChatCompletion(ctx, req)
 		if err != nil {
-			return "", err
+			return text.String(), err
 		}
+		text.WriteString(response.Choices[0].Delta.Content)
+	}
 
-		if len(resp.Choices) == 0 {
-			slog.Error("no response", slog.String("model", model), slog.Duration("duration", time.Since(completionStart)))
-			return "", errNoResponse
-		}
+	slog.Debug("response", slog.String("text", text.String()), slog.Duration("duration", time.Since(completionStart)))
 
-		slog.Debug("response", slog.String("text", resp.Choices[0].Message.Content), slog.Duration("duration", time.Since(completionStart)))
-		return resp.Choices[0].Message.Content, nil
-
-		/*
-			stream, err := client.CreateChatCompletionStream(ctx, req)
-			if err != nil {
-				return "", err
-			}
-			defer stream.Close()
-
-			var text strings.Builder
-			for {
-				response, err := stream.Recv()
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				if err != nil {
-					return text.String(), err
-				}
-				if len(response.Choices) > 0 {
-					text.WriteString(response.Choices[0].Delta.Content)
-				}
-			}
-
-			slog.Debug("response", slog.String("text", text.String()), slog.Duration("duration", time.Since(completionStart)))
-
-			l.Messages = append(l.Messages, azopenai.ChatCompletionMessage{
-				Role:    RoleAssistant,
-				Content: text.String(),
-			})
-			return text.String(), nil
-	*/
+	l.Messages = append(l.Messages, openai.ChatCompletionMessage{
+		Role:    RoleAssistant,
+		Content: text.String(),
+	})
+	return text.String(), nil
 }
