708476d9d1c10834e5c5711a0c8958ad3bfc2e3a
zeozeozeo <108888572+zeozeozeo@users.noreply.github.com>
2024-12-18 23:33:24 +0500

    replace mentions

--- main.go
+++ main.go
@@ -1057,6 +1057,9 @@ func readTxtCache(attachmentID snowflake.ID) ([]byte, bool) {
 
 func getMessageContent(message discord.Message, isWhitelisted bool) string {
 	content := message.Content
+	for _, mention := range message.Mentions {
+		content = strings.ReplaceAll(content, mention.Mention(), "@"+mention.EffectiveName())
+	}
 
 	// fetch from txt attachments, some of them may be cached on disk
 	for i, attachment := range message.Attachments {
